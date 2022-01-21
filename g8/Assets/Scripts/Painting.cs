287a2f06ba320b3559e7ceea2452f63ecc66a26b
Francisco Cecilio <ist189444@tecnico.ulisboa.pt>
2022-01-21 20:03:49 +0000

    Select and Move Paintings to walls

--- g8/Assets/Scripts/Painting.cs
+++ g8/Assets/Scripts/Painting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -11,45 +12,64 @@ public class Painting : MonoBehaviour
     // On Selected stuff
     [SerializeField] private Material semiTransparentMaterial;
     [SerializeField] private Material defaultMaterial;
-    BoxCollider collider;
 
+    // when paintings are prefabs we need to find another way to assign the debugtext
+    BoxCollider myCollider;
+    public SelectingManager sM;
 
+    public Text DebugText;
+    private bool selected = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<BoxCollider>();
-        defaultMaterial = GetComponent<Material>();
-        
+        myCollider = GetComponent<BoxCollider>();
+        defaultMaterial = GetComponent<Renderer>().material;
+        //StartCoroutine(ListObj());
+        //StartCoroutine(UnListObj());
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     // when the painting is selected, it becomes moveable until we press Grip again.
     public void OnSelectedCustom(SelectEnterEventArgs args){
-        Debug.Log(gameObject.name + " was selected.");
-
-        RaycastHit hit;
+        /*RaycastHit hit;
         if(((XRRayInteractor) args.interactorObject).TryGetCurrent3DRaycastHit(out hit)){
             Vector3 t = hit.point;
-        }
+        }*/
 
-        // desligar o collider
-        collider.enabled = false;
-        // mudar a transparência
+        // deactivate the collider
+        myCollider.enabled = false;
+        // change material
         GetComponent<Renderer>().material = semiTransparentMaterial;
 
-        // informar o Interaction Manager que o cubo esta a ser selecionado para ser movido
+        // The user has 5 seconds to pick a new position
+        selected = true;
+        StartCoroutine(DeselectTimeout());
 
-        //guardar o interactor 
+        // inform Selecting Manager of the painting being selected
+        sM.ListSelectedObject(gameObject);
+        
     }
 
+    // method called by the SelectingManager 
     public void OnDeselectedCustom(){
-        Debug.Log(gameObject.name + " was deselected.");
         GetComponent<Renderer>().material = defaultMaterial;
+        myCollider.enabled = true;
+        selected = false;
+    }
+
+    IEnumerator DeselectTimeout(){
+        yield return new WaitForSeconds(5);
+        if(selected)
+            sM.UnListObject();
+    }
+
+
+    public void OnHoverCustom(HoverEnterEventArgs args){
+        DebugText.text += "\n" + gameObject.name + " was hovered.";
     }
 }
--- g8/Assets/Scripts/SelectingManager.cs
+++ g8/Assets/Scripts/SelectingManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -8,34 +9,51 @@ public class SelectingManager : MonoBehaviour
 {
 
     public GameObject selectedObject;
-
+    public Text DebugText;
+    public XRRayInteractor interactor;
+    // Raycast Layer Mask - changes to green to validate targets
+    
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
     }
 
     public void ListSelectedObject(GameObject painting){
-        
+        DebugText.text += "\nObject listed: "+ painting.name;
         selectedObject = painting;
+        // now, we only want to select Walls
+        LayerMask mask = LayerMask.GetMask("Walls");
+        interactor.raycastMask = mask;
+    }
+    
+    public void UnListObject(){
+        DebugText.text += "\n" + selectedObject.name + " unlisted";
+        selectedObject.GetComponent<Painting>().OnDeselectedCustom();
+        selectedObject = null;
+        // replace the original layermask
+        interactor.raycastMask = LayerMask.GetMask("InteractibleObjects", "UI");
     }
 
-    public async void RightControllerTrigger(SelectEnterEventArgs args){
-        Debug.Log("right controller triggered.");
+
+    public void WallSelected(SelectEnterEventArgs args){
+        DebugText.text += "\nWall+selected: " + selectedObject.name;
         // If we have a painting ready to be moved
         if(selectedObject != null){
             RaycastHit hit;
             if(((XRRayInteractor) args.interactorObject).TryGetCurrent3DRaycastHit(out hit)){
-                Vector3 t = hit.point;
-                selectedObject.transform.position = t;
-                selectedObject.GetComponent<Painting>().OnDeselectedCustom();
-                Debug.Log("Objeto deselecionado.");
+                DebugText.text += "\n"+ hit.point + " " + selectedObject.name;
+                selectedObject.transform.position = hit.point;
+                selectedObject.transform.forward = -1*hit.normal;
+                
+                UnListObject();
+                
             }
         }
     }
