c6408ca3f4e77e6ec9e4d95dff67410102a2aef9
Francisco Cecilio <ist189444@tecnico.ulisboa.pt>
2022-01-25 16:14:18 +0000

    Main Menu (Piotr you could add a floor and a painting HERE)

---
+++ g8/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject optionsMenu;
+    public bool optionsActive = false;
+
+    public void NaturalScene(){
+        SceneManager.LoadScene("NatureGallery");
+    }
+
+    public void DarkScene(){
+        SceneManager.LoadScene("CosmicGallery");
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+
+    public void DisplayOptions(){
+        if(!optionsActive){
+            optionsActive = true;
+            optionsMenu.SetActive(true);
+        }
+        else{
+            optionsActive = false;
+            optionsMenu.SetActive(false);
+        }
+    }
+    
+}
