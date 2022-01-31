a0d8221a7e1f2241d3c6af0a57d2fa8757f5519b
Francisco Cecilio <ist189444@tecnico.ulisboa.pt>
2022-01-31 10:09:51 +0000

    GUI change art style

--- g8/Assets/Scripts/ButtonsManager.cs
+++ g8/Assets/Scripts/ButtonsManager.cs
@@ -9,4 +9,9 @@ public class ButtonsManager : MonoBehaviour
     public void MainMenu(){
         SceneManager.LoadScene("MainMenu");
     }
+
+    // reset game
+
+    // end game + see the results
+    
 }
--- g8/Assets/Scripts/WristMenu.cs
+++ g8/Assets/Scripts/WristMenu.cs
@@ -1,24 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class WristMenu : MonoBehaviour
 {
     public GameObject wristUI;
+    public Text artThemeText;
+    public GridElement[] cells;
+    public int artThemeCode = 0;
     public InputActionReference toggleReference = null; //for toggle UI Wrist
+
 
     void Awake()
     {
         toggleReference.action.started += ToggleWristUI;
+        UpdateGridCells();
+        UpdateArtText();
     }
-
+    
     void OnDestroy(){
         toggleReference.action.started -= ToggleWristUI;
     }
-
+    
     private void ToggleWristUI(InputAction.CallbackContext context){
         bool isActive = !gameObject.activeSelf;
         gameObject.SetActive(isActive);
+        if(isActive){
+            // Update Art Text
+            UpdateArtText();
+            // Update grid cells
+            UpdateGridCells();
+        }
+    }
+
+    public void UpdateGridCells(){
+        foreach(GridElement cell in cells){
+            cell.SetArt(artThemeCode);
+        }
+    }
+    
+    public void IncreaseThemeCode(){
+        if(artThemeCode >= 3){
+            artThemeCode = 0;
+        }
+        else{
+            artThemeCode++;
+        }
+        UpdateArtText();
+        UpdateGridCells();
+    }
+
+    public void DecreaseThemeCode(){
+        if(artThemeCode <= 0){
+            artThemeCode = 3;
+        }
+        else{
+            artThemeCode--;
+        }
+        UpdateArtText();
+        UpdateGridCells();
+    }
+
+    private void UpdateArtText(){
+        if(artThemeCode == 0){
+            artThemeText.text = "ANIMAL";
+        } 
+        else if(artThemeCode == 1){
+            artThemeText.text = "LANDSCAPE";
+        } 
+        else if(artThemeCode == 2){
+            artThemeText.text = "PSYCHO";
+        }
+        else if(artThemeCode == 3){
+            artThemeText.text = "DARK";
+        }
     }
 }
